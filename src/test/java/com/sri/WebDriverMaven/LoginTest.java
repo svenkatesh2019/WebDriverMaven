d67bef71c276b8a34fbb6a9c45012e5861dd0b9c
svenkatesh2019 <sridevivenkatesh@outlook.com>
2021-03-08 21:41:33 -0500

    added some code

--- src/test/java/com/sri/WebDriverMaven/LoginTest.java
+++ src/test/java/com/sri/WebDriverMaven/LoginTest.java
@@ -1,36 +1,41 @@
 package com.sri.WebDriverMaven;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
-	
+
 	public static WebDriver driver;
-	
+
 	@BeforeTest
-	public void setUp()
-	{
+	public void setUp() {
 		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
+		driver = new ChromeDriver();
+
+	}
+
+	@Test
+	public void login() {
+		driver.get("https://facebook.com");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);// Implicit wait
+		driver.findElement(By.id("email")).sendKeys("sud.v@hotmail.com");
+		driver.findElement(By.name("pass")).sendKeys("555555555555555");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
 		
 	}
-	
-	@Test
-	public void login()
-	{
-		driver.get("https://facebook.com");
-	}
-	
+
 	@AfterTest
-	public void tearDown()
-	{
+	public void tearDown() {
 		driver.quit();
 	}
 
